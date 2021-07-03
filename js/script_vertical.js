7bc9d0fdae11302a476cb9deb1046d0d21e97026
seoyeonpp <seoyeonpub@gmail.com>
2021-07-03 14:22:01 +0900

    jQuery 원페이지 스크롤

---
+++ js/script_vertical.js
@@ -0,0 +1,69 @@
+$(function(){
+    var $box = $('section'),
+        $nav = $('.indicator>a'),
+        current = 0,
+        $last = $box.length-1;
+    
+        $box.eq(current).animate({top:"0"},5); //시작하면 0번째 섹션 보이게
+
+    //휠이벤트
+    $(window).on('mousewheel',function(e){
+        var delta = e.originalEvent.wheelDelta; // -120, 120
+        if(delta<0){
+            if($box.is(':animated') == false){
+                if(current == $last){
+                    return; //current가 3이면 종료
+                }else{
+                    current ++; //1
+                    old = current-1; //0
+                    $box.eq(current).css({top:"100%"}).stop().animate({top:"0%"},500);
+                    $box.eq(old).stop().animate({top:"-100%"},500);
+                    $nav.eq(current).addClass('active');
+                    $nav.eq(old).removeClass('active');
+    
+                    console.log(`current : ${current}, old: ${old}`);
+                }
+            };
+            
+        }else{
+            if($box.is(':animated') == false){
+
+                if(current == 0){
+                    return; //current가 0이면 함수종료
+                }else{
+                    current --;
+                    old = current+1;
+                    $box.eq(current).css({top:"-100%"}).stop().animate({top:"0%"},500);
+                    $box.eq(old).stop().animate({top:"100%"},500);
+                    $nav.eq(current).addClass('active');
+                    $nav.eq(old).removeClass('active');
+                    console.log(`current : ${current}, old: ${old}`);
+                }
+
+            };
+        }
+    });
+
+
+    //인디케이터 클릭
+    $nav.on('click',function(){
+        var it = $(this).index();
+        console.log(`click : ${it} current : ${current}`);
+
+        $nav.removeClass('active');
+        $nav.eq(it).addClass('active');
+
+        if(it > current){
+            $box.eq(current).stop().animate({top:"-100%"},500);
+            $box.eq(it).css({top:"100%"}).stop().animate({top:"0%"},500);
+            current = it;
+        }else{
+            $box.eq(current).stop().animate({top:"100%"},500);
+            $box.eq(it).css({top:"-100%"}).stop().animate({top:"0%"},500);
+            current = it;
+        }
+
+        return false;
+    });
+
+});
